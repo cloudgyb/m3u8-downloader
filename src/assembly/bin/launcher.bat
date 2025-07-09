ade75a849af2449c4ad7c8e454f24186f3f9883f
cloudgyb <gyb_cloud@163.com>
2025-07-09 10:29:23 +0800

    build(assembly): 更新打包配置和启动脚本
    - 修改 assembly.xml 文件，添加 zip 和 tar.gz打包格式
    - 更新启动脚本，增加日志配置参数

--- src/assembly/bin/launcher.bat
+++ src/assembly/bin/launcher.bat
@@ -1,5 +1,4 @@
 @echo off
-set JLINK_VM_OPTIONS=
+set JLINK_VM_OPTIONS=-Dlogback.configurationFile=../conf/logback.xml
 set DIR=%~dp0
-set VM_OPTIONS=-Dlogback.configurationFile="%DIR%"\..\conf\logback.xml
-"%DIR%\javaw" %VM_OPTIONS% %JLINK_VM_OPTIONS% -m m3u8downloader/com.github.cloudgyb.m3u8downloader.M3u8DownloaderApp %*
+start "m3u8 downloader" "%DIR%\javaw" %JLINK_VM_OPTIONS% -m m3u8downloader/com.github.cloudgyb.m3u8downloader.M3u8DownloaderApp %*
