8c5a47470dd061234abd4dfb019ded5575a5f9ae
cloudgyb <gyb_cloud@163.com>
2025-07-31 13:45:01 +0800

    refactor(launcher): 修改启动脚本以确保正确的工作目录 - 更新 M3U8Downloader.exe 启动器 - 在启动脚本中添加 cd /d "%~dp0"命令，确保设置当前目录为脚本所在目录 - 这个修改解决了 M3U8Downloader.exe 通过浏览器启动不能生成数据库文件导致的无法下载问题

--- src/assembly/bin/launcher.bat
+++ src/assembly/bin/launcher.bat
@@ -1,4 +1,5 @@
 @echo off
 set JLINK_VM_OPTIONS=-Dlogback.configurationFile=../conf/logback.xml
 set DIR=%~dp0
+cd /d "%~dp0"
 start "m3u8 downloader" "%DIR%\javaw" %JLINK_VM_OPTIONS% -m m3u8downloader/com.github.cloudgyb.m3u8downloader.M3u8DownloaderApp %*
